2e51dd927f7f47cef743bf1e3b8f364cc46b64d2
jesmark <clasher.jesmark@gmail.com>
2016-12-13 09:16:44 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+	int a, b, c, d;
+	
+	
+	printf("Enterl value of a: ");
+	scanf("%d", &a);
+	printf("Enter value of b: ");
+	scanf("%d", &b);
+	printf("Enter value of c: ");
+	scanf("%d", &c);
+	printf("%lf",((a/3)+(b/3)+(c/3)));
+}
